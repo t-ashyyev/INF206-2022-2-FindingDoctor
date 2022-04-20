4d1821d10ce180d3b407037bfa6213ccd2315161
AmirulAhsan <81914088+AmirulAhsan@users.noreply.github.com>
2022-04-20 11:26:46 +0700

    Update loginPasien.blade.php

--- loginPasien.blade.php
+++ loginPasien.blade.php
@@ -1,24 +1,34 @@
 <!DOCTYPE html>
-
 <html lang="en">
- 
 <head>
-    <meta charset="utf-8">
-    <title>Login sebagai Pasien</title>
-    <meta name="description" content="slick Login">
-    <meta name="author" content="Webdesigntuts+">
-
-    <link rel="stylesheet" a href="/Css/loginPasien.css">
-   
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FindingDoctor</title>
+    <link rel="stylesheet" href="/Css/loginPasien.css">
+    <link rel="icon" href="/Logo/logo icon.jpeg" type="image/x-icon">
 </head>
- 
 <body>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <!-- Tabs Titles -->
+            <h2 class="active"> Pasien </h2>
+            
+        <!-- Icon -->
+        <div class="fadeIn first">
+            <img src="/Logo/Logo finding dokter.jpeg" alt="logo" width="150">
+        </div>
 
-    <form>
-        <input type="text" name="username">
-        <input type="password" name="password">
-        <input type="submit" value="Log In">
-    </form>
+        <!-- Login -->
+        <form>
+        <input type="text" id="login" class="fadeIn second" name="login" placeholder="pasien_id">
+        <input type="password" id="password" class="fadeIn third" name="login" placeholder="password">
+        <input type="submit" class="fadeIn fourth" value="Log In">
+        </form>
+        
+    </div>
+
+  </div>
+</div>
 </body>
-     
 </html>
