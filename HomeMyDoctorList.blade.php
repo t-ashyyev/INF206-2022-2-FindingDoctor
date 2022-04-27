f40766c41945f47493bcbc4929abe50adfe402d3
Zhillal <teuku_zm20@mhs.usyiah.ac.id>
2022-04-27 11:28:02 +0700

    Navbar Home My DoctorList

--- HomeMyDoctorList.blade.php
+++ HomeMyDoctorList.blade.php
@@ -7,6 +7,58 @@
     <title>MyDoctorList</title>
 </head>
 <body>
-    
+    <div class="wrapper fadeInDown">
+    <!-- ============ HEADER =========== -->
+    <header id="header">
+      <!-- ============ NAV =========== -->
+      <div class="wrapper fadeInDown">
+      <nav class="navbar navbar-expand-lg nav-box-shadow">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img
+              src="/Logo/logo1.png"
+              alt="Logo"
+              width="150"
+            />
+          </a>
+
+          <a href="#" id="btn-collapse">
+            <i class="bx bx-menu"></i>
+          </a>
+
+          <div class="collapse-nav" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a href="" class="nav-link">Info</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Find Doctor</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Recommendation</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">My Doctor List</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Settings</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Help</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Feedback</a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
 </body>
 </html>
