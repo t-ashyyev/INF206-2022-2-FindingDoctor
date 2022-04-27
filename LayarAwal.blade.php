bc27d1ee74ae331e1bb36103574c612be5dada21
Zhillal <teuku_zm20@mhs.usyiah.ac.id>
2022-04-27 10:49:10 +0700

    Layar Awal
    Layar awal baru

--- LayarAwal.blade.php
+++ LayarAwal.blade.php
@@ -6,28 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Css/LayarAwal.css">
     <title>FindingDoctor</title>
-    <link rel="icon" href="/Logo/Logo_title.png" type="image/x-icon">
+    <link rel="icon" href="/Logo/Logo_tittle.png" type="image/x-icon">
 </head>
 <body>
 <div class="wrapper fadeInDown">
-        <div id="formContent">
-            <!-- Tabs Titles -->
-            <h2 class="active"> SELAMAT DATANG </h2>
+    <div id="formContent">
+        <!-- Tabs Titles -->
+        <h2 class="active"> SELAMAT DATANG </h2>
 
         <!-- Icon -->
         <div class="fadeIn first">
             <img src="/Logo/logo1.png" alt="logo" width="150">
         </div>
 
+        <form>
+        <input type="text" id="login" class="fadeIn second" name="login" placeholder="Dokter_id">
+        <input type="password" id="password" class="fadeIn third" name="login" placeholder="password">
+        </form>
+
         <!-- Login -->
-       
-        <input type="button" class="fadeIn fourth" value="Log In Sebagai Pasien" onclick="window.location.href='/logPasien'">
-        <input type="button" class="fadeIn fourth" value="Log In Sebagai Dokter" onclick="window.location.href='/logDoctor'">
-        <input type="button" class="fadeIn fourth" value="Registrasi" onclick="window.location.href='/Registrasi'">
+        <input type="button" class="fadeIn fourth" value="Log In Sebagai Pasien" onclick="window.location.href='/loginPasien'">
+        <input type="button" class="fadeIn fourth" value="Log In Sebagai Dokter" onclick="window.location.href='/loginDokter'">
+        <input type="button" class="fadeIn fourth" value="Registrasi" onclick="window.location.href='/Regis'">
         
     </div>
-
-  </div>
 </div>
 </body>
 </html>
